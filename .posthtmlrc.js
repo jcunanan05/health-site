aa2d241dd8273e6bdf1c2ae1efc3e208589a1860
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-05-07 15:01:33 -0400

    Fix typo

--- .posthtmlrc.js
+++ .posthtmlrc.js
@@ -1,11 +1,11 @@
-const root = __dirname + 'src/pages';
+const root = __dirname + '/src/pages';
 
 const posthtmlConfig = {
   plugins: {
     'posthtml-include': {
       root
     },
-    'posthtml-extends': {
+    'posthtml-extend': {
       root
     }
   }
